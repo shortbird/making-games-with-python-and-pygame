36eebfcea9028d3afa9075430a764ed11e3bbc28
Tanner Bowman <tannerbowman@gmail.com>
2021-05-05 14:53:16 -0600

    Update wormy.py

--- wormy/wormy.py
+++ wormy/wormy.py
@@ -4,7 +4,9 @@
 # Released under a "Simplified BSD" license
 
 import random, pygame, sys
-from pygame.locals import *
+from pygame import *
+
+clock = pygame.time.Clock()
 
 FPS = 15
 WINDOWWIDTH = 640
@@ -42,8 +44,9 @@ def main():
 
     showStartScreen()
     while True:
-        runGame()
-        showGameOverScreen()
+      clock.tick(10)
+      runGame()
+      showGameOverScreen()
 
 
 def runGame():
@@ -59,52 +62,53 @@ def runGame():
     apple = getRandomLocation()
 
     while True: # main game loop
-        for event in pygame.event.get(): # event handling loop
-            if event.type == QUIT:
-                terminate()
-            elif event.type == KEYDOWN:
-                if (event.key == K_LEFT or event.key == K_a) and direction != RIGHT:
-                    direction = LEFT
-                elif (event.key == K_RIGHT or event.key == K_d) and direction != LEFT:
-                    direction = RIGHT
-                elif (event.key == K_UP or event.key == K_w) and direction != DOWN:
-                    direction = UP
-                elif (event.key == K_DOWN or event.key == K_s) and direction != UP:
-                    direction = DOWN
-                elif event.key == K_ESCAPE:
-                    terminate()
+      clock.tick(10)
+      for event in pygame.event.get(): # event handling loop
+          if event.type == QUIT:
+              terminate()
+          elif event.type == KEYDOWN:
+              if (event.key == K_LEFT or event.key == K_a) and direction != RIGHT:
+                  direction = LEFT
+              elif (event.key == K_RIGHT or event.key == K_d) and direction != LEFT:
+                  direction = RIGHT
+              elif (event.key == K_UP or event.key == K_w) and direction != DOWN:
+                  direction = UP
+              elif (event.key == K_DOWN or event.key == K_s) and direction != UP:
+                  direction = DOWN
+              elif event.key == K_ESCAPE:
+                  terminate()
 
-        # check if the worm has hit itself or the edge
-        if wormCoords[HEAD]['x'] == -1 or wormCoords[HEAD]['x'] == CELLWIDTH or wormCoords[HEAD]['y'] == -1 or wormCoords[HEAD]['y'] == CELLHEIGHT:
-            return # game over
-        for wormBody in wormCoords[1:]:
-            if wormBody['x'] == wormCoords[HEAD]['x'] and wormBody['y'] == wormCoords[HEAD]['y']:
-                return # game over
+      # check if the worm has hit itself or the edge
+      if wormCoords[HEAD]['x'] == -1 or wormCoords[HEAD]['x'] == CELLWIDTH or wormCoords[HEAD]['y'] == -1 or wormCoords[HEAD]['y'] == CELLHEIGHT:
+          return # game over
+      for wormBody in wormCoords[1:]:
+          if wormBody['x'] == wormCoords[HEAD]['x'] and wormBody['y'] == wormCoords[HEAD]['y']:
+              return # game over
 
-        # check if worm has eaten an apple
-        if wormCoords[HEAD]['x'] == apple['x'] and wormCoords[HEAD]['y'] == apple['y']:
-            # don't remove worm's tail segment
-            apple = getRandomLocation() # set a new apple somewhere
-        else:
-            del wormCoords[-1] # remove worm's tail segment
+      # check if worm has eaten an apple
+      if wormCoords[HEAD]['x'] == apple['x'] and wormCoords[HEAD]['y'] == apple['y']:
+          # don't remove worm's tail segment
+          apple = getRandomLocation() # set a new apple somewhere
+      else:
+          del wormCoords[-1] # remove worm's tail segment
 
-        # move the worm by adding a segment in the direction it is moving
-        if direction == UP:
-            newHead = {'x': wormCoords[HEAD]['x'], 'y': wormCoords[HEAD]['y'] - 1}
-        elif direction == DOWN:
-            newHead = {'x': wormCoords[HEAD]['x'], 'y': wormCoords[HEAD]['y'] + 1}
-        elif direction == LEFT:
-            newHead = {'x': wormCoords[HEAD]['x'] - 1, 'y': wormCoords[HEAD]['y']}
-        elif direction == RIGHT:
-            newHead = {'x': wormCoords[HEAD]['x'] + 1, 'y': wormCoords[HEAD]['y']}
-        wormCoords.insert(0, newHead)
-        DISPLAYSURF.fill(BGCOLOR)
-        drawGrid()
-        drawWorm(wormCoords)
-        drawApple(apple)
-        drawScore(len(wormCoords) - 3)
-        pygame.display.update()
-        FPSCLOCK.tick(FPS)
+      # move the worm by adding a segment in the direction it is moving
+      if direction == UP:
+          newHead = {'x': wormCoords[HEAD]['x'], 'y': wormCoords[HEAD]['y'] - 1}
+      elif direction == DOWN:
+          newHead = {'x': wormCoords[HEAD]['x'], 'y': wormCoords[HEAD]['y'] + 1}
+      elif direction == LEFT:
+          newHead = {'x': wormCoords[HEAD]['x'] - 1, 'y': wormCoords[HEAD]['y']}
+      elif direction == RIGHT:
+          newHead = {'x': wormCoords[HEAD]['x'] + 1, 'y': wormCoords[HEAD]['y']}
+      wormCoords.insert(0, newHead)
+      DISPLAYSURF.fill(BGCOLOR)
+      drawGrid()
+      drawWorm(wormCoords)
+      drawApple(apple)
+      drawScore(len(wormCoords) - 3)
+      pygame.display.update()
+      FPSCLOCK.tick(FPS)
 
 def drawPressKeyMsg():
     pressKeySurf = BASICFONT.render('Press a key to play.', True, DARKGRAY)
@@ -133,26 +137,27 @@ def showStartScreen():
     degrees1 = 0
     degrees2 = 0
     while True:
-        DISPLAYSURF.fill(BGCOLOR)
-        rotatedSurf1 = pygame.transform.rotate(titleSurf1, degrees1)
-        rotatedRect1 = rotatedSurf1.get_rect()
-        rotatedRect1.center = (WINDOWWIDTH / 2, WINDOWHEIGHT / 2)
-        DISPLAYSURF.blit(rotatedSurf1, rotatedRect1)
+      clock.tick(10)
+      DISPLAYSURF.fill(BGCOLOR)
+      rotatedSurf1 = pygame.transform.rotate(titleSurf1, degrees1)
+      rotatedRect1 = rotatedSurf1.get_rect()
+      rotatedRect1.center = (WINDOWWIDTH / 2, WINDOWHEIGHT / 2)
+      DISPLAYSURF.blit(rotatedSurf1, rotatedRect1)
 
-        rotatedSurf2 = pygame.transform.rotate(titleSurf2, degrees2)
-        rotatedRect2 = rotatedSurf2.get_rect()
-        rotatedRect2.center = (WINDOWWIDTH / 2, WINDOWHEIGHT / 2)
-        DISPLAYSURF.blit(rotatedSurf2, rotatedRect2)
+      rotatedSurf2 = pygame.transform.rotate(titleSurf2, degrees2)
+      rotatedRect2 = rotatedSurf2.get_rect()
+      rotatedRect2.center = (WINDOWWIDTH / 2, WINDOWHEIGHT / 2)
+      DISPLAYSURF.blit(rotatedSurf2, rotatedRect2)
 
-        drawPressKeyMsg()
+      drawPressKeyMsg()
 
-        if checkForKeyPress():
-            pygame.event.get() # clear event queue
-            return
-        pygame.display.update()
-        FPSCLOCK.tick(FPS)
-        degrees1 += 3 # rotate by 3 degrees each frame
-        degrees2 += 7 # rotate by 7 degrees each frame
+      if checkForKeyPress():
+          pygame.event.get() # clear event queue
+          return
+      pygame.display.update()
+      FPSCLOCK.tick(FPS)
+      degrees1 += 3 # rotate by 3 degrees each frame
+      degrees2 += 7 # rotate by 7 degrees each frame
 
 
 def terminate():
@@ -181,9 +186,10 @@ def showGameOverScreen():
     checkForKeyPress() # clear out any key presses in the event queue
 
     while True:
-        if checkForKeyPress():
-            pygame.event.get() # clear event queue
-            return
+      clock.tick(10)
+      if checkForKeyPress():
+          pygame.event.get() # clear event queue
+          return
 
 def drawScore(score):
     scoreSurf = BASICFONT.render('Score: %s' % (score), True, WHITE)
